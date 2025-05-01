d9b6c7c74feeac47569ad44dd6fee2a039bf941c
Laura-Cepeda <laura.cepedab@gmail.com>
2025-05-01 16:00:46 -0400

    Create lesson1.js

---
+++ Core-Javascript/lesson1.js
@@ -0,0 +1,25 @@
+// 1.Hello word
+console.log("Hello Word")
+
+//variable
+ var firstName= "jhon"
+ let lastName= "Smith"
+ console.log(firstName)
+
+ var age, dateOfBirth, sex
+ age= "5"
+ sex= "Male"
+ console.log(age)
+ age= "6"
+ console.log(age)
+
+ //constants
+ const occupation = "Engineer"
+ console.log(occupation)
+
+ //data types
+ var middleName = 'David' //string
+ var ageofBorther = 25   //number
+ var isHeMarried = false  //boolean
+ var yearsInMarriage = null // no value
+ var numberOfCars = undefined
