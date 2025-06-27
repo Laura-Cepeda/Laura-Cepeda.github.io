468e3a7c7f25885eac1fe7d8a9803e9125f662fe
Laura-Cepeda <169110699+Laura-Cepeda@users.noreply.github.com>
2025-06-27 12:25:59 -0400

    Add files via upload

---
+++ Selenium-Python/Print_and_variables.py
@@ -0,0 +1,36 @@
+# This script demonstrates a simple Python program that prints a greeting message to the console.
+"""
+Functionality:
+- Prints "Hello" to the console.
+- Assigns a string value to a variable and prints it.
+"""
+print("Hello")
+
+# Assign a string value to a variable
+#str = "Hello World"  # This variable stores a greeting message
+#print(str)
+
+a = 3
+print(a)
+
+# Assign a string value to a variable
+#str = "Hello World"  # This variable stores a greeting message
+#print(str)
+
+# Create three variables with different types of values
+#a = 3  # Integer value
+#b = 6.4  # Float value
+#c = "Great"  # String value
+# Create three variables with different types of values
+a, b, c = 3, 6.4, "Great"
+
+# Print the values of the variables diferent examples
+print("Value of a is " + str(a))
+print("{}{}".format("value is ", b))
+print(c)
+
+# Print the types of the variables a, b, and c
+print("Type of a is", type(a))  # Prints the type of variable a
+print("Type of b is", type(b))  # Prints the type of variable b
+print("Type of c is", type(c))  # Prints the type of variable c
+
